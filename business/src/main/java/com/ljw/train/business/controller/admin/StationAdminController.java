18b3db0a0d05745c1f1383cd53f6fa4eb01cf782
liujiawen <15232860385@163.com>
2026-01-12 10:06:53 +0800

    车次 站名下拉框优化

--- business/src/main/java/com/ljw/train/business/controller/admin/StationAdminController.java
+++ business/src/main/java/com/ljw/train/business/controller/admin/StationAdminController.java
@@ -10,6 +10,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/station")
 public class StationAdminController {
@@ -34,5 +36,12 @@ public class StationAdminController {
         stationService.delete(id);
         return new CommonResp<>();
     }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<StationQueryResp>> queryList() {
+        List<StationQueryResp> list = stationService.queryAll();
+        return new CommonResp<>(list);
+    }
+
 
 }
